ca77059559ffee0d7d926243b5c5c99cabf4ccb4
greensalttt <oversensethema@naver.com>
2024-09-09 20:49:00 +0900

    feat: 인덱스 화면

--- src/main/java/com/example/greenspringboot/cust/controller/CustController.java
+++ src/main/java/com/example/greenspringboot/cust/controller/CustController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 // 레스트컨트롤러는 서버간의 데이터를 통신할때만, 뷰를 반환할때는 일반 컨트롤러
 //@RestController
 @Controller
@@ -17,11 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustController {
 
     private final CustService custService;
-
-    @GetMapping("/test")
-    public String getHello(){
+    @GetMapping("/")
+    public String home() {
         return "index";
     }
+
     @PostMapping("/join")
     public String join(@RequestBody CustDto custDto) {
         String result = custService.join(custDto);
