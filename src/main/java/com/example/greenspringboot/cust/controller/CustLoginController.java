dacff973ab7078724caa384ac57524161ef4a3f5
greensalttt <oversensethema@naver.com>
2024-09-29 16:37:00 +0900

    feat: 인터셉터 toURL

--- src/main/java/com/example/greenspringboot/cust/controller/CustLoginController.java
+++ src/main/java/com/example/greenspringboot/cust/controller/CustLoginController.java
@@ -5,6 +5,7 @@ import com.example.greenspringboot.dto.CustDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,12 +35,11 @@ public class CustLoginController {
 
 
     @PostMapping("/loginPost")
-    public String loginPost(@RequestParam String cEmail, @RequestParam String cPwd, HttpSession session) {
+    public String loginPost(@RequestParam String cEmail, @RequestParam String cPwd, HttpSession session, Model model) {
 
         CustDto custDto = custService.login(cEmail, cPwd);
         if (custDto != null) {
             /* session 변수에 고객 번호랑 닉네임 저장 */
-
             session.setAttribute("cId", custDto.getCId());
             session.setAttribute("cName", custDto.getCName());
             session.setAttribute("cNm", custDto.getCNm());
@@ -48,20 +48,27 @@ public class CustLoginController {
             String formattedDate = custDto.getRegDt().format(formatter);
             session.setAttribute("regDt", formattedDate); // 포맷된 문자열을 세션에 저장
 
+            String toURL = (String) session.getAttribute("toURL");
+
+            /*이전 URL이 있으면 해당 페이지로 리다이렉트, 없으면 인덱스*/
+            toURL = (toURL != null && !toURL.isEmpty()) ? toURL : "/";
+
+            /*로그인 후에는 이전 URL을 세션에서 삭제합니다.*/
+            session.removeAttribute("toURL");
+
 
             System.out.println("Session cId: " + session.getAttribute("cId"));
             System.out.println("Session cName: " + session.getAttribute("cName"));
             System.out.println("Session cNm: " + session.getAttribute("cNm"));
             System.out.println("Session regDt: " + session.getAttribute("regDt"));
 
-            return "index";
-        }
-        return "errorPage";
+            return "redirect:" + toURL;
+    } else {
+        // 비밀번호가 틀렸을 경우 모델에 에러 메시지 추가
+        model.addAttribute("errorMessage", "이메일 또는 비밀번호가 잘못되었습니다.");
+        return "loginForm"; // 로그인 페이지로 포워딩
     }
-
-
-
-
+    }
 }
 
 
