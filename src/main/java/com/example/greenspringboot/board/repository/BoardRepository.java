d0d975539d38956e36aa64b2a3ffe9ccf33c89b1
greensalttt <oversensethema@naver.com>
2024-11-01 23:37:42 +0900

    feat: 게시판 레포

--- src/main/java/com/example/greenspringboot/board/repository/BoardRepository.java
+++ src/main/java/com/example/greenspringboot/board/repository/BoardRepository.java
@@ -1,6 +1,21 @@
 package com.example.greenspringboot.board.repository;
 
+import com.example.greenspringboot.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    // 제목 또는 내용을 키워드로 검색하는 메서드
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
+    // 검색 조건에 맞는 게시물의 개수를 세는 메서드
+    int countByTitleContainingOrContentContaining(String title, String content);
 }
+
+//
+//
+////      JPA 방식으로 사용할때 순서
+//
+////1. DB 테이블 > 엔티티 만들기 > DTO 만들기 > 레포 만들기 > 나머지 로직/
