7c2d74e475109db7ad805fc3c272de9553954492
greensalttt <oversensethema@naver.com>
2025-01-04 19:16:38 +0900

    feat: 댓글 dto

---
+++ src/main/java/com/example/greenspringboot/comment/dto/CommentDto.java
@@ -0,0 +1,21 @@
+package com.example.greenspringboot.comment.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+@Builder
+public class CommentDto {
+    private Integer cno;
+    private Integer cId;
+    private Integer bno;
+    private Integer pcno;
+    private String  comment;
+    private String  commenter;
+    private Date    regDate;
+    private Date    upDate;
+    private Integer deleted;
+
+
+}
