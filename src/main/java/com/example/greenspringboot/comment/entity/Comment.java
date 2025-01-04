1d0946080884f3d38d16747a328923b008ae21e8
greensalttt <oversensethema@naver.com>
2025-01-04 19:22:59 +0900

    feat: 댓글 엔티티

---
+++ src/main/java/com/example/greenspringboot/comment/entity/Comment.java
@@ -0,0 +1,55 @@
+package com.example.greenspringboot.comment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "comment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Comment {
+    //    pk값 설정
+    @Id
+//    오토 인크리먼트 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cno", nullable = false)
+    private Integer cno;
+
+    @Column(name="c_id", nullable = false)
+    private Integer cId;
+
+    @Column(name="bno", nullable = false)
+    private String bno;
+
+    @Column(name="pcno", nullable = false)
+    private String pcno;
+
+    @Column(name="comment", nullable = false)
+    private String comment;
+
+    @Column(name="commenter", nullable = false)
+    private String commenter;
+
+    @Builder.Default
+    @Column(name= "reg_dt", nullable = false)
+    private Date regDt = new Date();
+
+
+    @Builder.Default
+    @Column(name= "up_dt", nullable = false)
+//    private LocalDateTime upDt = LocalDateTime.now();
+    private Date upDt = new Date();
+
+    //    기본값 설정
+    @Builder.Default
+    @Column(name = "deleted", nullable = false)
+    private Boolean deleted = false; // 기본값 false
+
+}
