2b243bd318cc7ab83c9da687ae3104e8b3988b2a
greensalttt <oversensethema@naver.com>
2025-02-19 22:31:32 +0900

    chore: 비밀번호 찾기 초기 구상

--- src/main/java/com/example/greenspringboot/cust/controller/ForgotPwdController.java
+++ src/main/java/com/example/greenspringboot/cust/controller/ForgotPwdController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ForgotPwdController {
 
 
+
+/*
+1. 가입되있는 이메일인지 확인
+2. 그 이메일로 인증번호 보냄
+2. 인증번호 확인 후 비밀번호 변경
+3. 변경된 비밀번호로 로그인
+*/
     @GetMapping("/forgotPwd")
     public String forgotPwd() {
         return "forgotPwd";
