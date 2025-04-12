b9ed18110df971051a4b5029d6cc1337700c659c
greensalttt <oversensethema@naver.com>
2025-04-12 21:53:50 +0900

    test: 테스트 코드 추가

--- src/test/java/com/example/greenspringboot/board/BoardRepositoryTest.java
+++ src/test/java/com/example/greenspringboot/board/BoardRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.greenspringboot.board.service.BoardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 public class BoardRepositoryTest {
@@ -22,5 +23,13 @@ public class BoardRepositoryTest {
                     .build();
             boardService.write(boardDto);
         }
+    }
+
+    @Test
+    public void pwd(){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPwd = "1234";
+        String encodedPwd = encoder.encode(rawPwd);
+        System.out.println(encodedPwd);
     }
 }
