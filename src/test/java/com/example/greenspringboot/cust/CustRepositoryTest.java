672ff9cd6af8004e24e56c455ebce0491f87c2da
greensalttt <oversensethema@naver.com>
2025-05-27 16:41:31 +0900

    배포 test

--- src/test/java/com/example/greenspringboot/cust/CustRepositoryTest.java
+++ src/test/java/com/example/greenspringboot/cust/CustRepositoryTest.java
@@ -1,22 +1,24 @@
-package com.example.greenspringboot.cust;
-import com.example.greenspringboot.cust.repository.CustRepository;
-import com.example.greenspringboot.cust.service.CustService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
-public class CustRepositoryTest {
-
-    @Autowired private CustRepository custRepository;
-
-    @Autowired private CustService custService;
-
-
+//package com.example.greenspringboot.cust;
+//import com.example.greenspringboot.cust.dto.CustDto;
+//import com.example.greenspringboot.cust.entity.Cust;
+//import com.example.greenspringboot.cust.repository.CustRepository;
+//import com.example.greenspringboot.cust.service.CustService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//@SpringBootTest
+//public class CustRepositoryTest {
+//
+//    @Autowired private CustRepository custRepository;
+//
+//    @Autowired private CustService custService;
+//
+//
 //    @Test
 //    public void crudTest() {
 //        CustDto custDto = CustDto.builder()
@@ -41,37 +43,37 @@ public class CustRepositoryTest {
 //                .build();
 //        custRepository.save(cust);
 //    }
-
-//    이메일이 중복이면 실패, 중복 아니면 성공
-    @Test
-    public void emailCheckTest() {
-        boolean exists = custRepository.existsBycEmail("test@naver.com");
-        assertFalse(exists, "fali");
-    }
-
-    @Test
-    public void nickCheckTest() {
-        boolean exists = custRepository.existsBycNick("아이스2");
-        assertFalse(exists, "fali");
-    }
-
-
-    @Test
-    public void pwd(){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPwd = "1234";
-        String encodedPwd = encoder.encode(rawPwd);
-        System.out.println("해시값: "+encodedPwd);
-    }
-
-    @Test
-    public void checkPassword() {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-        String raw = "12345";
-        String hash = "$2a$10$AyJUTr2rXuY4jUe2RpGVReCD4/ylIak5/TyDG7GdHLvbjVTEpbuXC";
-
-        assertTrue(encoder.matches(raw, hash));
-    }
-
-}
+//
+////    이메일이 중복이면 실패, 중복 아니면 성공
+//    @Test
+//    public void emailCheckTest() {
+//        boolean exists = custRepository.existsBycEmail("test@naver.com");
+//        assertFalse(exists, "fali");
+//    }
+//
+//    @Test
+//    public void nickCheckTest() {
+//        boolean exists = custRepository.existsBycNick("아이스2");
+//        assertFalse(exists, "fali");
+//    }
+//
+//
+//    @Test
+//    public void pwd(){
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String rawPwd = "1234";
+//        String encodedPwd = encoder.encode(rawPwd);
+//        System.out.println("해시값: "+encodedPwd);
+//    }
+//
+//    @Test
+//    public void checkPassword() {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//        String raw = "12345";
+//        String hash = "$2a$10$AyJUTr2rXuY4jUe2RpGVReCD4/ylIak5/TyDG7GdHLvbjVTEpbuXC";
+//
+//        assertTrue(encoder.matches(raw, hash));
+//    }
+//
+//}
