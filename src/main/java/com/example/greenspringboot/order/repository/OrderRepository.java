fb6d0de8f983a2e7f05686c6bb595dc9811d7b17
greensalttt <oversensethema@naver.com>
2025-07-23 15:38:55 +0900

    feat: 예매 내역 조인 쿼리

--- src/main/java/com/example/greenspringboot/order/repository/OrderRepository.java
+++ src/main/java/com/example/greenspringboot/order/repository/OrderRepository.java
@@ -1,7 +1,41 @@
 package com.example.greenspringboot.order.repository;
 
+import com.example.greenspringboot.order.dto.MyReservationDto;
 import com.example.greenspringboot.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    Optional<Order> findByOrderId(String orderId);
+
+    @Modifying
+    @Query("DELETE FROM Order o WHERE o.status = :status AND o.createdAt < :time")
+    void cleanUpOrders(@Param("status") String status, @Param("time") LocalDateTime time);
+
+
+//    @Query("SELECT new com.example.greenspringboot.order.dto.MyReservationDto(" +
+//            "o.ono, o.orderId, p.pno, p.title, o.ticketCount, o.totalPrice, pay.paymentMethod, pay.createdAt, o.status) " +
+//            "FROM Order o LEFT JOIN o.payment pay LEFT JOIN o.performance p " +
+//            "WHERE o.status = 'paid' AND o.createdBy = :cId")
+//    List<MyReservationDto> findMyReservations(@Param("cId") Integer cId);
+
+
+    @Query("SELECT new com.example.greenspringboot.order.dto.MyReservationDto(" +
+            "o.ono, o.orderId, p.pno, p.title, p.artist, p.venue, p.date, o.ordererName, o.ticketCount, o.totalPrice, pay.paymentMethod, pay.createdAt, o.status) " +
+            "FROM Order o " +
+            "LEFT JOIN Payment pay ON o.ono = pay.ono " +  // 🔥 조인 조건 명시!
+            "LEFT JOIN o.performance p " +
+            "WHERE o.status = 'paid' AND o.createdBy = :cId")
+    List<MyReservationDto> findMyReservations(@Param("cId") Integer cId);
+
+
+
+
 }
